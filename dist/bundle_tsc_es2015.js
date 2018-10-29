7f94a1b6f1b533b07fdca24469a98e7f89d0e416
cheezenaan <katsuhiko.ito@repro.io>
2018-10-29 18:25:16 +0900

    bundle with tsc(target: es2015)

---
+++ dist/bundle_tsc_es2015.js
@@ -0,0 +1,55 @@
+var hoge = (function () {
+  'use strict';
+
+  var classCallCheck = function (instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  };
+
+  var createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  var Sound = function () {
+      function Sound() {
+          classCallCheck(this, Sound);
+      }
+
+      createClass(Sound, [{
+          key: 'euphonium',
+          value: function euphonium() {
+              return 'Sound! Euphonium';
+          }
+      }, {
+          key: 'find',
+          value: function find() {
+              return [1, 2, 3].find(function (n) {
+                  return n === 2;
+              });
+          }
+      }]);
+      return Sound;
+  }();
+
+  var sound = new Sound();
+  sound.euphonium();
+  sound.find();
+
+  return Sound;
+
+}());
