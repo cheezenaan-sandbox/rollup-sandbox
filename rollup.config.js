eb946e2f31a3cea9b2fb5753fd657165771b2bea
cheezenaan <katsuhiko.ito@repro.io>
2018-10-29 18:26:55 +0900

    bundle with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
 
 export default {
-  input: './src/index.ts',
+  input: './src/index.js',
   output: {
     file: './dist/bundle.js',
     format: 'iife',
     name: 'hoge'
   },
-  plugins: [nodeResolve(), commonjs(), typescript(), babel()]
+  plugins: [nodeResolve(), commonjs(), babel()]
 };
